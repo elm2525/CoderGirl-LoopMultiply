ace0bb2f67f86ba925314b403902c74f8aa16029
Erin Moore <erin.moore726@gmail.com>
2019-03-24 21:42:21 -0500

    Loop Multiply in progress

--- LoopMultiply/Program.cs
+++ LoopMultiply/Program.cs
@@ -7,12 +7,42 @@ namespace LoopMultiply
         public static void Main()
         {
             Console.Write("Enter a number: ");
+            Console.WriteLine();
+
+            int sum = 0;
+            int result = 0;
+
+            for (int number = 2; number < 21; number++)
+            {
+                if (number > 0)
+                {
+                    result = number * number++;
+                    Console.WriteLine(result);
+
+                }
+            }
+
+            /*if (result > 0)
+            {
+                sum = 
+                Console.WriteLine(number);
+                    
+            }
+            */        
+
+           
+
+            Console.ReadLine();    
+        }
+
+          
+            
 
             // TODO: Loop from 2 to 20 and multiply the loop value by the number the user entered and output the value each time. 
 
             // TODO: When the loop is finished, output the sum of all previous values.
 
-            Console.ReadLine();
-        }
+           
+    
     }
 }
